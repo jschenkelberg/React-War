09f75d8f9d5c87a0c16b8bc8650e5c11bc6ed55e
jschenkelberg <jmschenkelberg@gmail.com>
2021-07-14 16:12:13 -0500

    built header

---
+++ src/components/app.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Header from './Header';
+
+class App extends Component {
+    state = {  }
+    
+    render() { 
+        return (  
+            <div className="container-fluid">
+                <Header />
+            </div>
+        );
+    }
+}
+ 
+export default App;
